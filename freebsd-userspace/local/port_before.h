1543249d3eb742a3dc470e4976ed44074f701bc0
Joel Sherrill <joel.sherrill@oarcorp.com>
2012-07-11 17:54:57 -0500

    port_before.h: Add _ALIGNBYTES and enable getservent.c

--- freebsd-userspace/local/port_before.h
+++ freebsd-userspace/local/port_before.h
@@ -10,3 +10,14 @@
 #ifndef __ssize_t
 #define __ssize_t ssize_t
 #endif
+
+/*
+ * lib/libc/net/getservent.c needs _ALIGNBYTES and there seems to be no
+ * clean way to get it from the FreeBSD kernel code.
+ *
+ * Duplicated from freebsd/sys/param.h
+ */
+#ifndef _ALIGNBYTES
+#define _ALIGNBYTES     (sizeof(long) - 1)
+#endif
+

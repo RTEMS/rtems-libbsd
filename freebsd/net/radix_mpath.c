734ccdaf97a6597e3457c0090bda73b449e333a6
Joel Sherrill <joel.sherrill@oarcorp.com>
2012-09-05 12:52:06 -0500

    freebsd/net/radix_mpath.c: Fix prototype/body mismatch
    The routines which had bodies not matching the prototype are:
      - rn_mpath_count
      - rtalloc_mpath_fib

--- freebsd/net/radix_mpath.c
+++ freebsd/net/radix_mpath.c
@@ -79,7 +79,11 @@ rn_mpath_next(struct radix_node *rn)
 		return NULL;
 }
 
+#ifdef __rtems__
+u_int32_t
+#else
 uint32_t
+#endif
 rn_mpath_count(struct radix_node *rn)
 {
 	uint32_t i = 0;
@@ -261,7 +265,11 @@ different:
 }
 
 void
+#ifdef __rtems__
+rtalloc_mpath_fib(struct route *ro, u_int32_t hash, u_int fibnum)
+#else
 rtalloc_mpath_fib(struct route *ro, uint32_t hash, u_int fibnum)
+#endif
 {
 	struct radix_node *rn0, *rn;
 	u_int32_t n;

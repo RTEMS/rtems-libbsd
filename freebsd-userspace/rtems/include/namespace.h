f25ce7ab7d5d57b88739f5b043a4132f97cac2d6
Joel Sherrill <joel.sherrill@oarcorp.com>
2012-09-07 12:43:13 -0500

    libc namespace.h: Use RTEMS specific versions
    FreeBSD adds an _ prefix to a number of libc symbols so they
    can switch from threaded to non-threaded libraries. We don't
    have that distinction and don't need this.
    
    Unfortunately, they also explicitly use the _ prefixed names
    of some methods. So we fix this in our version of namespace.h

---
+++ freebsd-userspace/rtems/include/namespace.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) XXX
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef _NAMESPACE_H_
+#define _NAMESPACE_H_
+
+/*
+ * Reverse the use of "hidden" names in the source code.
+ *
+ * README: When modifying this file don't forget to make the appropriate
+ *         changes in un-namespace.h!!!
+ */
+
+
+#define _pthread_getspecific		pthread_getspecific
+#define _pthread_key_create		pthread_key_create
+#define _pthread_main_np		pthread_main_np
+#define _pthread_once			pthread_once
+#define _pthread_setspecific		pthread_setspecific
+#define _read				read
+#define _recvfrom			recvfrom
+#define _sendto				sendto
+#define _setsockopt			setsockopt
+#define _socket				socket
+
+#define _close				close
+#define _connect			connect
+#define _writev				writev
+#define _getpeername			getpeername
+#define _getprogname			getprogname
+#define _getsockname			getsockname
+
+
+
+#endif /* _NAMESPACE_H_ */

db068df3c40419b8d2588235055ec4bffbd5f114
Joel Sherrill <joel.sherrill@oarcorp.com>
2012-09-07 12:49:40 -0500

    netshell01: Link against ping command

--- testsuite/netshell01/shellconfig.c
+++ testsuite/netshell01/shellconfig.c
@@ -4,11 +4,13 @@
 
 #include <rtems/shell.h>
 
+#include <rtems/netcmds-config.h>
+
 #define CONFIGURE_SHELL_COMMANDS_INIT
 #define CONFIGURE_SHELL_COMMANDS_ALL
-#define CONFIGURE_SHELL_MOUNT_MSDOS
-#define CONFIGURE_SHELL_MOUNT_RFS
-#define CONFIGURE_SHELL_DEBUGRFS
+
+#define CONFIGURE_SHELL_USER_COMMANDS \
+    &rtems_shell_PING_Command
 
 #include <rtems/shellconfig.h>
 

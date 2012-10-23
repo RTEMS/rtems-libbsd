8ddbc30f4156d870f005387faac1676aaaeca33c
Joel Sherrill <joel.sherrill@oarcorp.com>
2012-10-23 14:21:34 -0500

    rtems-bsd-sx.c: Disable check for interruptible blocking until it is supported

--- rtemsbsd/src/rtems-bsd-sx.c
+++ rtemsbsd/src/rtems-bsd-sx.c
@@ -194,7 +194,8 @@ _sx_xlock(struct sx *sx, int opts, const char *file, int line)
 {
 	rtems_status_code sc = RTEMS_SUCCESSFUL;
 
-	BSD_ASSERT((opts & SX_INTERRUPTIBLE) == 0);
+	#warning "SX_INTERRUPTIBLE NOT SUPPORTED YET"
+	/* BSD_ASSERT((opts & SX_INTERRUPTIBLE) == 0); */
 
 	sc = rtems_semaphore_obtain( sx->lock_object.lo_id, RTEMS_WAIT, RTEMS_NO_TIMEOUT);
 	BSD_ASSERT_SC(sc);

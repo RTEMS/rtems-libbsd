89050cca02f291cea1073bbac55c3b2248f44a43
Sebastian Huber <sebastian.huber@embedded-brains.de>
2015-09-01 07:48:34 +0200

    Delete superfluous legacyvar.h files

--- libbsd.py
+++ libbsd.py
@@ -1736,14 +1736,6 @@ def pci(mm):
         ]
     )
     mod.addTargetSourceCPUDependentHeaderFiles(
-        [ 'arm', 'avr', 'bfin', 'h8300', 'lm32', 'm32c', 'm32r', 'm68k', 'mips',
-          'nios2', 'powerpc', 'sh', 'sparc', 'sparc64', 'v850' ],
-        'i386',
-        [
-		'sys/i386/include/legacyvar.h',
-        ]
-    )
-    mod.addTargetSourceCPUDependentHeaderFiles(
         [ 'arm', 'avr', 'bfin', 'h8300', 'i386', 'lm32', 'm32c', 'm32r', 'm68k',
           'mips', 'nios2', 'powerpc', 'sh', 'sparc', 'sparc64', 'v850' ],
         'x86',

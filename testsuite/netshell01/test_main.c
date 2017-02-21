631fb983aa2e5731973c4b4df677cb59e0bf6606
Sebastian Huber <sebastian.huber@embedded-brains.de>
2017-02-21 12:57:34 +0100

    netshell01: Add license, fix format

--- testsuite/netshell01/test_main.c
+++ testsuite/netshell01/test_main.c
@@ -1,4 +1,30 @@
 /*
+ * COPYRIGHT (c) 2012. On-Line Applications Research Corporation (OAR).
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
  *  This is the body of the test. It does not do much except ensure
  *  that the target is alive after initializing the TCP/IP stack.
  */
@@ -13,17 +39,15 @@
 
 #define TEST_NAME "LIBBSD NETSHELL 1"
 
-/*
- * RTEMS Startup Task
- */
-static void test_main(void)
+static void
+test_main(void)
 {
-  rtems_shell_env_t env;
+	rtems_shell_env_t env;
 
-  memset(&env, 0, sizeof(env));
-  rtems_shell_main_loop( &env );
+	memset(&env, 0, sizeof(env));
+	rtems_shell_main_loop(&env);
 
-  exit( 0 );
+	exit(0);
 }
 
 #include <rtems/bsd/test/default-init.h>

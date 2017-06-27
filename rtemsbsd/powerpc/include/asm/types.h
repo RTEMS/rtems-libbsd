65d6fa20fb0dccaa9770c7e8b496d90b683fb288
Sebastian Huber <sebastian.huber@embedded-brains.de>
2017-06-27 09:31:11 +0200

    linux: Make Linux types available in user-space

--- rtemsbsd/powerpc/include/asm/types.h
+++ rtemsbsd/powerpc/include/asm/types.h
@@ -31,8 +31,6 @@
 #ifndef	_ASM_TYPES_H_
 #define	_ASM_TYPES_H_
 
-#ifdef _KERNEL
-
 typedef uint8_t u8;
 typedef uint8_t __u8;
 typedef uint16_t u16;
@@ -56,7 +54,5 @@ typedef vm_paddr_t dma_addr_t;
 typedef vm_paddr_t dma64_addr_t;
 
 typedef unsigned short umode_t;
-
-#endif	/* _KERNEL */
 
 #endif	/* _ASM_TYPES_H_ */

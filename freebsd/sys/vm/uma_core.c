ea121a0c59373e50e0409e9158a164b9ff531cae
Sebastian Huber <sebastian.huber@embedded-brains.de>
2018-01-16 13:06:39 +0100

    ZONE(9): Avoid function call overhead

--- freebsd/sys/vm/uma_core.c
+++ freebsd/sys/vm/uma_core.c
@@ -99,12 +99,14 @@ __FBSDID("$FreeBSD$");
 #include <ddb/ddb.h>
 #ifdef __rtems__
   #ifdef RTEMS_SMP
+    #include <rtems/score/smp.h>
+
     /*
      * It is essential that we have a per-processor cache, otherwise the
      * critical_enter()/critical_exit() protection would be insufficient.
      */
     #undef curcpu
-    #define curcpu rtems_get_current_processor()
+    #define curcpu _SMP_Get_current_processor()
     #undef mp_maxid
     #define mp_maxid (rtems_get_processor_count() - 1)
     #define SMP

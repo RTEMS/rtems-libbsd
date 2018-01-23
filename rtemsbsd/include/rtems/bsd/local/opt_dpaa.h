2fba1e4322f2aa1f8060d4e5cea47a3e5f2236bb
Sebastian Huber <sebastian.huber@embedded-brains.de>
2018-01-23 10:48:19 +0100

    dpaa: Remove unused configuration defines
    Update #3277.

--- rtemsbsd/include/rtems/bsd/local/opt_dpaa.h
+++ rtemsbsd/include/rtems/bsd/local/opt_dpaa.h
@@ -9,12 +9,3 @@
 #undef __ppc_generic
 #define __ppc_generic
 #define KBUILD_MODNAME "dpaa"
-#define CONFIG_FSL_BMAN
-#define CONFIG_FSL_BMAN_PORTAL
-#define CONFIG_FSL_BMAN_TEST_API
-#define CONFIG_FSL_BMAN_TEST_THRESH
-#define CONFIG_FSL_QMAN
-#define CONFIG_FSL_QMAN_CONFIG
-#define CONFIG_FSL_QMAN_PORTAL
-#define CONFIG_FSL_QMAN_TEST_API
-#define CONFIG_FSL_QMAN_TEST_STASH

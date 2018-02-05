9906c4a5eea85e70980b7ea8bbf0f3e30e8cf85c
Sebastian Huber <sebastian.huber@embedded-brains.de>
2018-02-05 12:05:52 +0100

    tests: Fix configuration dependencies

--- testsuite/timeout01/init.c
+++ testsuite/timeout01/init.c
@@ -39,6 +39,9 @@
 
 #include "timeout_test.h"
 
+uintptr_t rtems_bsd_allocator_domain_page_mbuf_size =
+    RTEMS_BSD_ALLOCATOR_DOMAIN_PAGE_MBUF_DEFAULT;
+
 static void Init(rtems_task_argument arg)
 {
 	rtems_status_code sc;

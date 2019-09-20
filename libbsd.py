54ded2228908664fff6f5cc409993800b9d071ee
Sebastian Huber <sebastian.huber@embedded-brains.de>
2019-09-20 07:51:19 +0200

    PCI(4): Import missing file

--- libbsd.py
+++ libbsd.py
@@ -2520,9 +2520,10 @@ class pci(builder.Module):
         mm = self.manager
         self.addKernelSpaceSourceFiles(
             [
+                'sys/dev/pci/pcib_support.c',
                 'sys/dev/pci/pci.c',
-                'sys/dev/pci/pci_user.c',
                 'sys/dev/pci/pci_pci.c',
+                'sys/dev/pci/pci_user.c',
             ],
             mm.generator['source']()
         )

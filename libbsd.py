eb4dcdf76de663e0cfa145737f03353568945cbd
Sebastian Huber <sebastian.huber@embedded-brains.de>
2019-09-20 13:53:06 +0200

    NVME(4): Install header files
    Update #3821.

--- libbsd.py
+++ libbsd.py
@@ -126,6 +126,7 @@ _defaults = {
      ('freebsd/sys/vm',                         '**/*.h',                          'vm'),
      ('freebsd/sys/dev/mii',                    '**/*.h',                          'dev/mii'),
      ('freebsd/sys/dev/pci',                    '**/*.h',                          'dev/pci'),
+     ('freebsd/sys/dev/nvme',                   '**/*.h',                          'dev/nvme'),
      ('linux/include',                          '**/*.h',                          ''),
      ('mDNSResponder/mDNSCore',                 'mDNSDebug.h',                     ''),
      ('mDNSResponder/mDNSCore',                 'mDNSEmbeddedAPI.h',               ''),

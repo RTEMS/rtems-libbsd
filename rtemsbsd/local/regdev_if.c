24806eec0954cac70d8f7e23d5046b21c94f5e60
Christian Mauderer <christian.mauderer@embedded-brains.de>
2020-02-13 10:56:26 +0100

    regulator: Add to build.
    Update #3869.

---
+++ rtemsbsd/local/regdev_if.c
@@ -0,0 +1,27 @@
+#include <machine/rtems-bsd-kernel-space.h>
+
+/*
+ * This file is produced automatically.
+ * Do not modify anything in here by hand.
+ *
+ * Created from source file
+ *   freebsd-org/sys/dev/extres/regulator/regdev_if.m
+ * with
+ *   makeobjops.awk
+ *
+ * See the source file for legal information
+ */
+
+#include <sys/param.h>
+#include <sys/queue.h>
+#include <sys/kernel.h>
+#include <sys/kobj.h>
+#include <sys/types.h>
+#include <dev/ofw/ofw_bus.h>
+#include <machine/bus.h>
+#include <rtems/bsd/local/regdev_if.h>
+
+struct kobjop_desc regdev_map_desc = {
+	0, { &regdev_map_desc, (kobjop_t)regdev_default_ofw_map }
+};
+

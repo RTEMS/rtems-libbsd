64c45b5a1ae864f069eee98ee5be49629294025d
Sebastian Huber <sebastian.huber@embedded-brains.de>
2020-04-14 15:55:33 +0200

    usb_busdma: Add USB_NEED_BUSDMA_COHERENT_ALLOC
    Some BSPs have a cache-coherent memory area, however, it should not be
    used for the USB controller.

--- rtemsbsd/include/rtems/bsd/local/opt_usb.h
+++ rtemsbsd/include/rtems/bsd/local/opt_usb.h
@@ -1,10 +1,15 @@
 #include <rtems/bsd/modules.h>
+#include <bsp.h>
 
 #define USB_HAVE_CONDVAR 1
 
 #define USB_HAVE_UGEN 1
 
 #define USB_HAVE_BUSDMA 1
+
+#if !defined(LIBBSP_ARM_IMX_BSP_H) && !defined(LIBBSP_ARM_STM32H7_BSP_H)
+#define USB_NEED_BUSDMA_COHERENT_ALLOC 1
+#endif
 
 #define USB_HAVE_COMPAT_LINUX 0
 

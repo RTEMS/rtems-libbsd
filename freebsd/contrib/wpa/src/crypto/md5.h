33bfaee89aa71d2252eb48d6b9a9ec17183faced
Christian Mauderer <oss@c-mauderer.de>
2020-10-09 21:50:22 +0200

    wpa: Fix multiple definition of `hmac_md5`
    hmac_md5 is defined in dhcpcd and in wpa supplicant.

--- freebsd/contrib/wpa/src/crypto/md5.h
+++ freebsd/contrib/wpa/src/crypto/md5.h
@@ -11,6 +11,10 @@
 
 #define MD5_MAC_LEN 16
 
+#ifdef __rtems__
+#define hmac_md5_vector	wpa_hmac_md5_vector
+#define hmac_md5	wpa_hmac_md5
+#endif /* __rtems__ */
 int hmac_md5_vector(const u8 *key, size_t key_len, size_t num_elem,
 		    const u8 *addr[], const size_t *len, u8 *mac);
 int hmac_md5(const u8 *key, size_t key_len, const u8 *data, size_t data_len,

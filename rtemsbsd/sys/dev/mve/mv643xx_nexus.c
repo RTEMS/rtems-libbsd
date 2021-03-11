c305316a6a35ed24216af44fd5f3712700dffe21
till straumann <till.straumann@alumni.tu-berlin.de>
2021-03-11 17:46:20 +0100

    mv643xx_nexus.c: removed trailing blanks

--- rtemsbsd/sys/dev/mve/mv643xx_nexus.c
+++ rtemsbsd/sys/dev/mve/mv643xx_nexus.c
@@ -18,19 +18,19 @@
  * new implementation and is the original work by the author.
  */
 
-/* 
+/*
  * Authorship
  * ----------
  * This software (mv643xx ethernet driver for RTEMS) was
  *     created by Till Straumann <strauman@slac.stanford.edu>, 2005-2007,
  * 	   Stanford Linear Accelerator Center, Stanford University.
- * 
+ *
  * Acknowledgement of sponsorship
  * ------------------------------
  * The 'mv643xx ethernet driver for RTEMS' was produced by
  *     the Stanford Linear Accelerator Center, Stanford University,
  * 	   under Contract DE-AC03-76SFO0515 with the Department of Energy.
- * 
+ *
  * Government disclaimer of liability
  * ----------------------------------
  * Neither the United States nor the United States Department of Energy,
@@ -39,18 +39,18 @@
  * completeness, or usefulness of any data, apparatus, product, or process
  * disclosed, or represents that its use would not infringe privately owned
  * rights.
- * 
+ *
  * Stanford disclaimer of liability
  * --------------------------------
  * Stanford University makes no representations or warranties, express or
  * implied, nor assumes any liability for the use of this software.
- * 
+ *
  * Stanford disclaimer of copyright
  * --------------------------------
  * Stanford University, owner of the copyright, hereby disclaims its
  * copyright and all other rights in this software.  Hence, anyone may
- * freely use it for any purpose without restriction.  
- * 
+ * freely use it for any purpose without restriction.
+ *
  * Maintenance of notices
  * ----------------------
  * In the interest of clarity regarding the origin and status of this
@@ -59,9 +59,9 @@
  * or distributed by the recipient and are to be affixed to any copy of
  * software made or distributed by the recipient that contains a copy or
  * derivative of this software.
- * 
+ *
  * ------------------ SLAC Software Notices, Set 4 OTT.002a, 2004 FEB 03
- */ 
+ */
 
 /* Nexus port by Till Straumann, <till.straumann@psi.ch>, 3/2021 */
 

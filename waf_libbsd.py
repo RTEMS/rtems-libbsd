29f07bd4ba4d82f1ddc0ac3dff9e8d85b52ff07a
Chris Johns <chrisj@rtems.org>
2026-07-04 15:14:01 +1000

    waf: Fix python escaping in waf_libbsd.py
    This is reported with Python 3.13.3.
    
    Closes #93

--- waf_libbsd.py
+++ waf_libbsd.py
@@ -349,7 +349,7 @@ class Builder(builder.ModuleManager):
                         if value != net_cfg[rtag]['value']:
                             updated = True
                             net_cfg[rtag]['value'] = value
-        transpose = [(':', '\:'), ('/', '\/')]
+        transpose = [(':', '\\:'), ('/', '\\/')]
         sed = 'sed '
         for tag in net_cfg:
             tv = ''
